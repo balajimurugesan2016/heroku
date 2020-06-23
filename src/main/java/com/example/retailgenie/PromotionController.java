1ff81780abd353c06aba8a260a71ff2866b0270b
Balaji Vengatesh Murugesan <balaji.vengatesh.murugesan@sap.com>
2020-06-23 18:18:22 +0000

    Adding from to

--- src/main/java/com/example/retailgenie/PromotionController.java
+++ src/main/java/com/example/retailgenie/PromotionController.java
@@ -1,6 +1,8 @@
 package com.example.retailgenie;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,10 +26,11 @@ return "testapp";
 }
 
 @GetMapping("/getpromotions")
-public String promotions(){
+@ResponseBody
+public String promotions(@RequestParam(required = true) Integer from, @RequestParam(required = true) Integer to){
 
-
-return "promotions";
+int c = from  + to;
+return Integer.toString(c);
 
 
 }
