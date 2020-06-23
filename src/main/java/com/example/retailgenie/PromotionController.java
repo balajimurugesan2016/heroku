bb075e16dc7d774a4d0f6a7800201b851d86154c
Balaji Vengatesh Murugesan <balaji.vengatesh.murugesan@sap.com>
2020-06-23 17:53:42 +0000

    new file

---
+++ src/main/java/com/example/retailgenie/PromotionController.java
@@ -0,0 +1,30 @@
+package com.example.retailgenie;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class PromotionController{
+
+
+    @GetMapping("/")
+public String firstTest(){
+
+   return "App is working";
+    }
+
+
+@GetMapping("/testapp")
+public String test(){
+
+
+return "testapp";
+
+
+}
+
+
+
+
+
+}
