82c98f21e93eab60b701b645b7d685eeb0e89187
Balaji Vengatesh Murugesan <balaji.vengatesh.murugesan@sap.com>
2020-07-03 09:40:38 +0000

    adding database

---
+++ src/main/java/com/example/retailgenie/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.example.retailgenie;
+import com.zaxxer.hikari.*;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Bean
+  public DataSource dataSource() {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+  }
+}
