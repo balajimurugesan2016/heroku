2c00c020a3e24e30df93a96718206c50ec2f38fa
Balaji Vengatesh Murugesan <balaji.vengatesh.murugesan@sap.com>
2020-07-04 05:00:57 +0000

    add controls

--- src/main/java/com/example/retailgenie/PromotionController.java
+++ src/main/java/com/example/retailgenie/PromotionController.java
@@ -2,6 +2,7 @@ package com.example.retailgenie;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,6 +68,16 @@ repository.save(promotion);
 return "test okay";
     
 }
+
+@DeleteMapping("/promotion")
+@ResponseBody
+public String deletePromotion(@RequestParam(required = true) Long promotionid){
+
+    repository.deleteById(promotionid);
+
+    return "Deleted";
+}
+
 
 
 
